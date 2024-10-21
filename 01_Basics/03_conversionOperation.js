88c358ec1243daaf25bcda346bac22985d9d2081
Himanshi Giri <76847888+himanshi-giri@users.noreply.github.com>
2024-10-21 19:26:09 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "33";
+//console.log(typeof score);
+
+let scoreInNumber = Number(score);
+//console.log(typeof scoreInNumber); 
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 1
+console.log(isLoggedIn)
+let BooleanLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false ; "karan" => true
+
