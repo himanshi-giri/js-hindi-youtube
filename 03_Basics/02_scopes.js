6a6f5b246e171516cc2c0b29e9c3b58f05dd8056
Himanshi Giri <76847888+himanshi-giri@users.noreply.github.com>
2024-10-27 10:10:08 +0000

    scope-update

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -11,10 +11,39 @@ if(true)
     let a = 10
     const b=20
     //var c=30
-    console.log(`inner ${a}`); //block scope
+    //console.log(`inner ${a}`); //block scope
     
 }
 //global scope
-console.log(a) // a will not be print as it goes out of scope (it is let)
+//console.log(a) // a will not be print as it goes out of scope (it is let)
 //console.log(b) // b will alsso not be printed out of the scope
 //console.log(c) // but c being var type will be printed even outside of the scope
+
+
+function one(){
+    const username = "Himanshi"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);        
+    }
+    //console.log(website);
+
+    two()    
+}
+
+one()
+
+//++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++
+
+addOne(5) //it'll work fine
+function addOne(num){ // normal function
+    return num+1
+}
+//addOne(5)
+
+console.log(addTwo(5)) //here it will give error , due to function hoisting
+const addTwo = function(num1){ // a variable holding a function
+    return num1+2
+}
+//console.log(addTwo(5))
