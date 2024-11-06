98d64d6e8f462f96e657be9432654f0b7f895d1a
Himanshi Giri <76847888+himanshi-giri@users.noreply.github.com>
2024-11-06 01:55:52 +0000

    five.js added

--- 05_iterators/five.js
+++ 05_iterators/five.js
@@ -1,5 +1,41 @@
 //forEach loop
 //foreach uses callback functions , what  is callback functions ? CallBack functions :- A function to execute for each element in the array. Its return value is discarded.
 const coding = ["js","cpp","py","ruby" , "java"]
-coding
+//coding.forEach(  function (item){ //callback function doesnot have a name 
+ //console.log(item)
+//})
 
+//callback function can also be an arrow function
+//coding.forEach((item)=>{
+//    console.log(item);
+    
+//})
+function print(item){
+    //console.log(item);
+}
+coding.forEach(print)
+
+coding.forEach((item,index,arr)=>{
+ //console.log(item,index,arr);
+})
+
+const myCoding =[
+    {
+        languageName : "javascript",
+        languageFileName:"js"
+    },
+    {
+        languageName : "java",
+        languageFileName:"java"
+    },
+    {
+        languageName : "python",
+        languageFileName:"py"
+    }
+]
+myCoding.forEach((item)=>{
+ console.log(item.languageName);
+ console.log("*+*+*+*+*+"); 
+ console.log(item.languageFileName);
+ 
+})
